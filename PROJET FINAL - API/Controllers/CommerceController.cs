6bace451c6e6d1f29c312e69c11fc868b18615d9
quentin guillou <quentin.guillou49@gmail.com>
2022-04-05 10:52:09 -0400

    Création du Controller des dépenses

---
+++ PROJET FINAL - API/Controllers/CommerceController.cs
@@ -0,0 +1,11 @@
+﻿using PROJET_FINAL___API.Logics.Controleurs;
+using PROJET_FINAL___API.Logics.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace PROJET_FINAL___API.Controllers
+{
+    [ApiController]
+    public class CommerceController : Controller
+}
--- PROJET FINAL - API/Controllers/DepenseController.cs
+++ PROJET FINAL - API/Controllers/DepenseController.cs
@@ -6,11 +6,54 @@ using System.Collections.Generic;
 
 namespace PROJET_FINAL___API.Controllers
 {
+    [ApiController]
     public class DepenseController : Controller
     {
-        public IActionResult Index()
+        [Route("Depense/ObtenirListeDepense")]
+        [HttpGet]
+        public List<DepenseDTO> ObtenirListeDepartement(string nomGarderie)
         {
-            return View();
+            List<DepenseDTO> listeDepense;
+            try
+            {
+                listeDepense = DepenseControleur.Instance.ObtenirListeDepense(nomGarderie);
+            }
+            catch (Exception)
+            {
+                listeDepense = new List<DepenseDTO>();
+            }
+            return listeDepense;
+        }
+
+
+        [Route("Depense/ObtenirDepense")]
+        [HttpGet]
+        public DepenseDTO ObtenirDepense([FromQuery] string nomGarderie, [FromQuery] string dateTemps)
+        {
+            DepenseDTO depense = new DepenseDTO(nomGarderie, dateTemps);
+            try
+            {
+                depense = DepenseControleur.Instance.ObtenirDepense(nomGarderie, dateTemps);
+            }
+            catch (Exception ex)
+            {
+                depense = new DepenseDTO(nomGarderie, dateTemps);
+            }
+            return depense;
+        }
+
+        [Route("Depense/AjouterDepense")]
+        [HttpPost]
+        public void AjouterDepense([FromQuery] string nomGarderie, [FromBody] DepenseDTO depenseDTO)
+        {
+            try
+            {
+                DepenseControleur.Instance.AjouterDepense(nomGarderie, depenseDTO);
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
